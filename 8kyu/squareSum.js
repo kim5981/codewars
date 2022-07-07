2066756e81c02677b9477e816a27a7698dcefb58
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-06 17:43:48 -0700

    solved

--- 8kyu/squareSum.js
+++ 8kyu/squareSum.js
@@ -2,3 +2,13 @@
 
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
+function squareSum(numbers){
+    let res = numbers.map(n => n * n )
+    .reduce((total, val) => {
+    return total + val
+    }, 0)  
+   
+  return res
+  }
+
+console.log(squareSum([1, 2, 2]))
