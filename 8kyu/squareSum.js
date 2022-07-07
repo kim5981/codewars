a9d4863036ce1f4d3b2d8310ef0b08e755a34eb3
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-06 20:14:15 -0700

    solved

--- 8kyu/squareSum.js
+++ 8kyu/squareSum.js
@@ -1,6 +1,8 @@
-// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+// Complete the square sum function so that it squares each number passed into it
+//and then sums the results together.
 
-// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+// For example, for [1, 2, 2] it should return 9 
+//because 1^2 + 2^2 + 2^2 = 9.
 
 function squareSum(numbers){
     let res = numbers.map(n => n * n )
