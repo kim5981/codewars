5e3bb5eed9bba46a866078cdc5409ac0a4f3a4aa
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-06 20:50:28 -0700

    new

---
+++ removeEveryOther.js
@@ -0,0 +1,11 @@
+/**
+ * Take an array and remove every second element from the array
+ * Always keep the first element and start removing with the next element.
+ * 
+ * Example:
+ * ["Keep", "Remove", "Keep", "Remove", "Keep", ...] 
+ * 
+ * becomes --> ["Keep", "Keep", "Keep", ...]
+ * 
+ */
+
