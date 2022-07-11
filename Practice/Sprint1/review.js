f43510417f40ff336f1fcb77487088fb161a481d
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-11 13:46:02 -0700

    codesignal 1.1

--- Practice/Sprint1/review.js
+++ Practice/Sprint1/review.js
@@ -41,3 +41,35 @@ function solution(n) {
     return total
 }
 
+
+/**
+ * From the year you were born until now is a certain number of years. 
+ * What year was it that many years before your birth year?
+
+For example, if a person was born in 2000 and it is now 2021, their "flip year" would be 1979.
+(Because they are 21 years old, and there are 21 years between 1979 and 2000.)
+ */
+
+function solution(birthYear, currentYear) {
+    let age = currentYear - birthYear
+    let flipYear = birthYear - age
+    return flipYear
+}
+
+
+/**
+ * Write a function that computes the sum of all values between n and m, inclusive.
+
+There is a formula you can use to compute the sum from 1 to n:
+
+total = n * (n + 1) / 2
+But we've misplaced the formula for the sum from n to m!
+
+Think outside the box... is there some way to use that formula, n, and m to get the answer? 
+You don't have to come up with a new formula; there's a way to use the existing one to solve it.
+ */
+
+function solution(n, m) { 
+    return ((m-n) + 1) * (m+n) / 2
+}
+
