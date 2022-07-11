30d34c9d686bd55c3f5a15c3b53da693f68a974c
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-11 13:42:05 -0700

    codesignal

---
+++ Practice/Sprint1/review.js
@@ -0,0 +1,11 @@
+// Write a function that returns the sum of two numbers.
+
+ function solution(arg1, arg2) {
+    return arg1 + arg2
+}
+
+
+// Write a function that returns the product of two numbers.
+function solution(arg1, arg2) {
+    return arg1 * arg2
+}
