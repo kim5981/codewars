15fdb8ba54438af3f30707d1a9aa3e4cc8564e5f
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-11 13:43:20 -0700

    codesignal

--- Practice/Sprint1/review.js
+++ Practice/Sprint1/review.js
@@ -9,3 +9,35 @@
 function solution(arg1, arg2) {
     return arg1 * arg2
 }
+
+/**
+ * Write a function that performs a sequence of mathematical operations by reassigning the results into the same variable.
+
+Even though this could be a one-liner, only use a single additional variable for this, and do each step as an independent operation.
+
+Add 7 to the number.
+Divide the result by 2 (the result could be fractional).
+Multiply that result by 5.
+Return the result.
+ */
+
+function solution(n) {
+    n = n + 7
+    n = n / 2
+    n = n * 5
+    return n
+}
+
+/**
+ * Write a function that computes the sum of all values between 1 and n, inclusive.
+
+There is a formula you can use to compute this:
+
+total = n * (n + 1) / 2
+ */
+
+function solution(n) {
+    let total = n * (n + 1) / 2
+    return total
+}
+
