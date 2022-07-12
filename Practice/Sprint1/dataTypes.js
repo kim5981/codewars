5e66a78f912558d33b628b5d5fd3b76db2fd560b
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-12 16:46:52 -0700

    reduce practice

--- Practice/Sprint1/dataTypes.js
+++ Practice/Sprint1/dataTypes.js
@@ -35,4 +35,37 @@ function solution(f, places) {
  * Write a function that returns true if a number is between 2 and 7,
  * exclusive. (That is, between 3 and 6, inclusive.)
  */
+ function solution(n) {
+    return n > 2 && n < 7 ? true : false
+ }
+
+ /**
+  * Normally, dividing by zero is a bad thing that will cause programs to crash.
+  *  Write a safer division function that returns the division of two numbers
+  *  unless the denominator is 0, in which case it should return 0 instead of crashing.
+ */
+
+  function solution(numerator, denominator) {
+    return denominator === 0 ? 0 : numerator/denominator 
+}
+
+/** 
+ * Return the largest of six numbers.
+
+Do not use any built-in maximum value function to solve this!
+*/
+
+function solution(a, b, c, d, e, f) {
+    let nums = [a, b, c, d, e, f]
+    let result = nums.reduce((acc, n) => {
+        console.log(`this is n: ${n}`)
+        console.log(`this is acc: ${acc}`)
+        return acc === null || n > acc ? n : acc
+    }, null) // starting at null for negative values
+    return result
+}
+
+
+console.log(solution(1,2,3,4,5,6))
+
 
