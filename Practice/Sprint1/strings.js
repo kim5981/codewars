8a71cbea968da97354212f0c6e8367ee8ff1f8eb
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-13 18:52:23 -0700

    factorial

---
+++ Practice/Sprint1/strings.js
@@ -0,0 +1,19 @@
+/**
+ * Compute the factorial of an integer n. This is commonly written as n!, which is read as "n factorial".
+
+You must use a loop to solve this problem. You cannot hard-code the answers.
+
+The factorial of a number is the product of every integer between 1 and n.
+
+Special case: the factorial of 0 is defined to be 1.
+ */
+
+function factorial(n) {
+    let current = 1;
+    for (let i = 1; i <= n; i++) {
+       current *= i;
+    }
+    return current
+}
+
+console.log(factorial(5))
