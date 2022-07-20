1ba9597924961ae0ac6ea206392d7ac02ab1aea1
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-20 13:02:48 -0700

    string copies

--- Practice/Sprint2/arrays.js
+++ Practice/Sprint2/arrays.js
@@ -13,3 +13,52 @@ function occurrencesOfV(a, v){
     }
     return counter
 }
+
+
+/**
+ * Construct a string of p copies of n capital "A"s and m capital "B"s.
+ * 
+ * Except if the copy number is evenly divisible by 3, in which case insert a string of p capital "X"s instead.
+ * 
+ * Copy numbers begin with 0 for the first copy.
+ * 
+ * Do not use any built-in string duplication functionality to solve this.
+ * 
+ * Example:
+ * 
+ * Input: 
+ * p = 3   n = 1    m = 1
+ * 
+ * Output:
+ * "XXXABAB"  (would be "ABABAB", but the one in the 0th place is evenly divisible by 3)
+ * 
+ */
+
+ function makeString(num, l){
+    let str = ""
+    for(let i=0; i<num; i++){
+        str += l
+    }
+    return str
+}
+
+function copiesOfStrings(p, n, m) {
+    let strA = makeString(n, "A")
+   
+    let strB = makeString(m, "B")
+    
+    let strAB = strA + strB
+    
+    let strX = makeString(p, "X")
+   
+    let strPAB = ""
+    
+    for(let i=0; i < p; i++){
+        if(i % 3 === 0)
+            strPAB +=  strX; 
+        else 
+        strPAB += strAB;
+    }
+    
+    return strPAB;   
+}
