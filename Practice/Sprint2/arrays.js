bc5f6860d150cf2816b0c90ad0c4358c8007797d
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-20 13:00:13 -0700

    occurrences of v

--- Practice/Sprint2/arrays.js
+++ Practice/Sprint2/arrays.js
@@ -1,2 +1,15 @@
 // Return the number of occurrences of v in array a
 // If the array is empty, return 0.
+
+function occurrencesOfV(a, v){
+    if(a.length < 1){
+        return 0
+    }
+    
+    let counter = 0
+    for(let i = 0; i < a.length; i++){
+        if(a[i] === v)
+        counter += 1
+    }
+    return counter
+}
