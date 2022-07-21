e7f9e2fa4258ca30282e0fd13e2ee52e40fbd722
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-20 21:08:49 -0700

    new

--- Practice/Sprint2/arrays.js
+++ Practice/Sprint2/arrays.js
@@ -112,3 +112,21 @@ function nStringOfA(n, m){
     let arr = Array(n).fill(str)
     return arr
 }
+
+/**
+ * You are given an array of positive integers arr. 
+ * You'd like to know how many triangles can be formed with side lengths equal to adjacent elements from arr.
+ *  
+ * Construct an array of integers of length arr.length - 2, 
+ *  where the ith element is equal to 1 if it's possible to form a triangle
+ * with side lengths arr[i], arr[i + 1], and arr[i + 2], otherwise 0.
+ * 
+ * Return the resulting array of integers.
+ * 
+ * Note: A triangle can be formed with side lengths a, b, and c if:
+ * 
+ *  a + b > c,   a + c > b,     and b + c > a.
+ * 
+ * Ex:
+ * For arr = [1, 2, 2, 4], the output should be solution(arr) = [1, 0]
+*/
