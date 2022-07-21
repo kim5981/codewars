1d9ce9cbd014962f68f85a23ef00fb3bbe39f930
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-20 23:21:16 -0700

    digits

--- Practice/Sprint2/arrays.js
+++ Practice/Sprint2/arrays.js
@@ -199,3 +199,23 @@ function shiftsSolution(elements) {
     // still hasn't gotten in order after loop, can return - 1
     return -1
 }
+
+/**
+ * You are given an array of integers digits representing the digits of a positive integer.
+ * 
+ * For example, digits = [1, 2, 3] represents the integer 123.
+ * 
+ * It is guaranteed that digits does not have any leading zeros.
+ * 
+ * Assuming that digits represents the integer n, 
+ * your task is to return an array that represents the integer n + 1.
+ * 
+ * Example 1:
+ * digits = [1, 2, 3]
+ * solution(digits) = [1, 2, 4]
+ * 
+ * Example 2:
+ * digits = [9, 9, 9]
+ * solution(digits) = [1, 0, 0, 0]
+ */
+
