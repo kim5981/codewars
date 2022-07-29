26d27a8ebfc270aac587a2fbcb27a8252e52dadf
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-29 15:34:00 -0700

    starsBorder

--- Practice/Sprint2/practice2.js
+++ Practice/Sprint2/practice2.js
@@ -60,3 +60,47 @@ function identityMatrix(size) {
     }
     return array
 }
+
+
+/**
+ * There is a bug in one line of the code. Find it, fix it, and submit.
+ * 
+ * Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+ * 
+ * Example:
+ * 
+ * Input:
+ * 
+ * picture = ["abc",
+              "ded"]
+
+ * Output:
+    solution(picture) = ["*****",
+                         "*abc*",
+                         "*ded*",
+                         "*****"]    
+ */
+
+function starsBorder(picture){
+    function solution(picture) {
+
+        var answer = [''];
+      
+        for (var i = 0; i < picture[0].length; i++) {
+          answer[0] += '*';
+        }
+      
+        for (var i = 0; i < picture.length; i++) {
+          answer.push('*');
+          for (var j = 0; j < picture[0].length; j++) {
+            answer[i + 1] += picture[i][j];
+          }
+          answer[i + 1] += '*';
+        }
+      
+        answer.push(answer[0]);
+      
+        return answer;
+      }
+      
+}
