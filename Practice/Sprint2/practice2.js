f360a31db8a92093281bfb851752c18dca1cdc15
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-29 15:29:12 -0700

    identityMatrix

--- Practice/Sprint2/practice2.js
+++ Practice/Sprint2/practice2.js
@@ -1,4 +1,6 @@
 /**
+ * -- 2D ARRAY 
+ * 
  * Create a 2D array of size row_count x col_count. 
  * Fill the array with 0.
  * 
@@ -19,6 +21,41 @@ function twoDimensionalArray(rowCount, colCount) {
     const array = []
     for(let i = 0; i < rowCount; i++){
         let row = Array(colCount).fill(0)
+        array.push(row)
+    }
+    return array
+}
+
+/**
+ * -- IDENTITY MATRIX
+ * 
+ * Create a square 2D array of size size x size. Fill the array with 0.
+ * 
+ * Then draw a line of 1s down the main diagonal. (an identity matrix)
+ * 
+ * Example:
+ * 
+ * Input:   size == 4
+ * 
+ * Output:
+ * 
+ * [
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1]
+]
+ * 
+ */
+
+function identityMatrix(size) {
+    const array = []
+    for(let i = 0; i < size; i++){
+        const row = []
+        for(let j = 0; j < size; j++){
+            if(i === j) row.push(1); // both start at same #, where they intersect is where you place 1
+            else row.push(0)
+        }
         array.push(row)
     }
     return array
