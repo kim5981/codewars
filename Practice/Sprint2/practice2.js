5a20422864daa5f4579eda8e2f99422ec87ad753
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-29 17:54:43 -0700

    substring

--- Practice/Sprint2/practice2.js
+++ Practice/Sprint2/practice2.js
@@ -106,3 +106,22 @@ function starsBorder(picture){
       
 }
 
+/**
+ * Return a substring (part of a string) between two indexes.
+ * The substring from index a up to (but not including) index b should be returned.
+ * 
+ * If a is less than 0, treat it like 0.
+ * If b is greater than the length of the string, treat it like the length of the string.
+ * If b is less than a, return an empty string.
+ * 
+ * Ex:
+ * Input:
+
+    s == "abcde"
+    a == 2
+    b == 4
+
+ * Output:  "cd"
+ */
+
+    
