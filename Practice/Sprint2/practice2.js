530f1f1a8f51298c96e56759ef7ceb2e8a5afff6
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-29 18:00:09 -0700

    multiplication

--- Practice/Sprint2/practice2.js
+++ Practice/Sprint2/practice2.js
@@ -167,4 +167,16 @@ function numberOfDigits(num){
  * 
  */
 
+function multiplicationTable(n){
+    const outerArr = []
     
+    for(let i = 1; i <= n; i++){
+        const row = []
+        
+        for(let j=1; j <= n; j++){
+            row.push(i*j)
+        }
+        outerArr.push(row)
+    }
+    return outerArr
+}    
